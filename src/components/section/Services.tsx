44cce2fefbadb743734fbc3353786b37d0d456c5
Arth Kulkarni <arthteerth93@gmail.com>
2025-07-05 20:53:41 +0530

    added cloudinary images

--- src/components/section/Services.tsx
+++ src/components/section/Services.tsx
@@ -33,31 +33,31 @@ export default function Services() {
 
                 {/* Cards */}
                 <div className="relative h-90 md:h-96 w-70 md:w-72 flex-shrink-0 ml-16 md:ml-20 hover:-translate-y-8 transition-transform duration-300 z-20">
-                    <img className="h-full w-full object-cover rounded-lg" src="src/assets/DSA.png" alt="Core Subject" />
+                    <img className="h-full w-full object-cover rounded-lg" src="https://res.cloudinary.com/dh626d9by/image/upload/v1751728709/DSA1_mmxh1m.png" alt="Core Subject" />
                     <span className="absolute p-6 inset-0 flex text-white text-18 leading-tight tracking-tight drop-shadow-lg">Data Structures & Algo</span>
                     <span className="absolute hidden md:block inset-y-14 inset-x-6 flex text-[#a3a5ae] text-15 tracking-tight leading-snug justify-text drop-shadow-lg">Master problem-solving techniques and optimize code efficiency.</span>
                 </div>
 
                 <div className="relative h-90 md:h-96 w-70 md:w-72 flex-shrink-0 hover:-translate-y-8 transition-transform duration-300 z-20">
-                    <img className="h-full w-full object-cover rounded-lg" src="src/assets/Core Subject.png" alt="DSA" />
+                    <img className="h-full w-full object-cover rounded-lg" src="https://res.cloudinary.com/dh626d9by/image/upload/v1751728723/core_kyaeol.png" alt="DSA" />
                     <span className="absolute p-6 inset-0 flex text-white text-18 leading-tight tracking-tight drop-shadow-lg">Core Subjects</span>
                     <span className="absolute hidden md:block inset-y-14 inset-x-6 flex text-[#a3a5ae] text-15 tracking-tight leading-snug justify-text drop-shadow-lg">Strengthen technical foundations with our curated core subject modules for knowledge building.</span>
                 </div>
 
                 <div className="relative h-90 md:h-96 w-70 md:w-72 flex-shrink-0 hover:-translate-y-8 transition-transform duration-300 z-20">
-                    <img className="h-full w-full object-cover rounded-lg" src="src/assets/premium.png" alt="Core Subject 2" />
+                    <img className="h-full w-full object-cover rounded-lg" src="https://res.cloudinary.com/dh626d9by/image/upload/v1751728685/premium1_jjz5ib.png" alt="Core Subject 2" />
                     <span className="absolute p-6 inset-0 flex text-white text-18 leading-tight tracking-tight drop-shadow-lg">Premium</span>
                     <span className="absolute hidden md:block inset-y-14 inset-x-6 flex text-[#a3a5ae] text-15 tracking-tight leading-snug justify-text drop-shadow-lg"></span>
                 </div>
 
                 <div className="relative h-90 md:h-96 w-70 md:w-72 flex-shrink-0 hover:-translate-y-8 transition-transform duration-300 z-20">
-                    <img className="h-full w-full object-cover rounded-lg" src="src/assets/DSA.png" alt="DSA 2" />
+                    <img className="h-full w-full object-cover rounded-lg" src="https://res.cloudinary.com/dh626d9by/image/upload/v1751728709/DSA1_mmxh1m.png" alt="DSA 2" />
                     <span className="absolute p-6 inset-0 flex text-white text-18 leading-tight tracking-tight drop-shadow-lg">System Design</span>
                     <span className="absolute hidden md:block inset-y-14 inset-x-6 flex text-[#a3a5ae] text-15 tracking-tight leading-snug justify-text drop-shadow-lg">Architect scalable solutions and design robust systems using our systematic approach.</span>
                 </div>
 
                 <div className="relative h-90 md:h-96 w-70 md:w-72 flex-shrink-0 opacity-80 -mr-10 md:-mr-20 hover:-translate-y-8 transition-transform duration-300 z-20">
-                    <img className="h-full w-full object-cover rounded-lg" src="src/assets/Core Subject.png" alt="Core Subject 3" />
+                    <img className="h-full w-full object-cover rounded-lg" src="https://res.cloudinary.com/dh626d9by/image/upload/v1751728723/core_kyaeol.png" alt="Core Subject 3" />
                     <span className="absolute p-6 inset-0 flex text-white text-18 leading-tight tracking-tight drop-shadow-lg">Aptitude</span>
                     <span className="absolute hidden md:block inset-y-14 inset-x-6 flex text-[#a3a5ae] text-15 tracking-tight leading-snug justify-text drop-shadow-lg">Enhance logical reasoning and quantitative skills through our structured development program.</span>
                 </div>
